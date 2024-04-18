41f84899fb298cff5fc61256557bf1f58e6d62c5
Siddhartha Rajbongshi <84823234+paras3c@users.noreply.github.com>
2024-04-18 19:24:57 +0000

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Siddhartha Rajbongshi",
+  title: "Cyber Security Practitioner and Open Source Advocate",
+  email: "Siddhartha.Rajbongshi@studentambassadors.com",
+  gitHub: "paras3c",
+  instagram: "siddhartha_rajbongshi",
+  linkedIn: "siddhartharajbongshi",
+  medium: "mrsiddhartha",
+  twitter: "siddharthastwt",
+  youTube: "xiddhartharajbongshi",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/cyyber.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "futuristic cyber themed background";
 
 /**
  * Project list
@@ -31,28 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Deep-dive into APIs : Theoretical aspects",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Wrote a blog on deep diving into APIs.",
+    url: "https://mrsiddhartha.hashnode.dev/deep-dive-into-apis-theoretical-aspects",
   },
   {
-    title: "Web Development for Beginners",
+    title: "HTTP, HTTPS, and APIs: Understanding the Relationship",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Wrote a blog on API basics.",
+    url: "https://mrsiddhartha.hashnode.dev/http-https-and-apis-understanding-the-relationship",
   },
 ];
 
